fe67898188de951263e52541ad640fabab5b945d
Giang Nguyen <hoanggia@uni-bremen.de>
2023-06-09 14:43:44 +0200

    Update universim_knowledge

---
+++ universim_ws/src/universim/universim_knowledge/USD/plugin/usdOntology/tokens.h
@@ -0,0 +1,101 @@
+//
+// Copyright 2016 Pixar
+//
+// Licensed under the Apache License, Version 2.0 (the "Apache License")
+// with the following modification; you may not use this file except in
+// compliance with the Apache License and the following modification to it:
+// Section 6. Trademarks. is deleted and replaced with:
+//
+// 6. Trademarks. This License does not grant permission to use the trade
+//    names, trademarks, service marks, or product names of the Licensor
+//    and its affiliates, except as required to comply with Section 4(c) of
+//    the License and to reproduce the content of the NOTICE file.
+//
+// You may obtain a copy of the Apache License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the Apache License with the above modification is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the Apache License for the specific
+// language governing permissions and limitations under the Apache License.
+//
+#ifndef USDONTOLOGY_TOKENS_H
+#define USDONTOLOGY_TOKENS_H
+
+/// \file UsdOntology/tokens.h
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// 
+// This is an automatically generated file (by usdGenSchema.py).
+// Do not hand-edit!
+// 
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+#include "pxr/pxr.h"
+#include "./api.h"
+#include "pxr/base/tf/staticData.h"
+#include "pxr/base/tf/token.h"
+#include <vector>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+
+/// \class UsdOntologyTokensType
+///
+/// \link UsdOntologyTokens \endlink provides static, efficient
+/// \link TfToken TfTokens\endlink for use in all public USD API.
+///
+/// These tokens are auto-generated from the module's schema, representing
+/// property names, for when you need to fetch an attribute or relationship
+/// directly by name, e.g. UsdPrim::GetAttribute(), in the most efficient
+/// manner, and allow the compiler to verify that you spelled the name
+/// correctly.
+///
+/// UsdOntologyTokens also contains all of the \em allowedTokens values
+/// declared for schema builtin attributes of 'token' scene description type.
+/// Use UsdOntologyTokens like so:
+///
+/// \code
+///     gprim.GetMyTokenValuedAttr().Set(UsdOntologyTokens->rdfConceptName);
+/// \endcode
+struct UsdOntologyTokensType {
+    USDONTOLOGY_API UsdOntologyTokensType();
+    /// \brief "rdf:conceptName"
+    /// 
+    /// UsdOntologyRdfAPI
+    const TfToken rdfConceptName;
+    /// \brief "rdf:namespace"
+    /// 
+    /// UsdOntologyRdfAPI
+    const TfToken rdfNamespace;
+    /// \brief "semanticTag:semanticLabel"
+    /// 
+    /// UsdOntologySemanticTagAPI
+    const TfToken semanticTagSemanticLabel;
+    /// \brief "Ontology"
+    /// 
+    /// Schema identifer and family for UsdOntologyOntology
+    const TfToken Ontology;
+    /// \brief "RdfAPI"
+    /// 
+    /// Schema identifer and family for UsdOntologyRdfAPI
+    const TfToken RdfAPI;
+    /// \brief "SemanticTagAPI"
+    /// 
+    /// Schema identifer and family for UsdOntologySemanticTagAPI
+    const TfToken SemanticTagAPI;
+    /// A vector of all of the tokens listed above.
+    const std::vector<TfToken> allTokens;
+};
+
+/// \var UsdOntologyTokens
+///
+/// A global variable with static, efficient \link TfToken TfTokens\endlink
+/// for use in all public USD API.  \sa UsdOntologyTokensType
+extern USDONTOLOGY_API TfStaticData<UsdOntologyTokensType> UsdOntologyTokens;
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif
---
+++ universim_ws/src/universim/universim_knowledge/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Check if USD_SRC_PATH or USD_BUILD_PATH exists
+if [ -z "$USD_SRC_PATH" ] || [ -z "$USD_BUILD_PATH" ]; then
+    echo "USD_SRC_PATH or USD_BUILD_PATH is unset." >&2
+    exit 1
+else
+    echo "USD_SRC_PATH is set to: $USD_SRC_PATH"
+    echo "USD_BUILD_PATH is set to: $USD_BUILD_PATH"
+
+    cp -r USD/plugin $USD_SRC_PATH
+
+    # Specify the file path
+    USD_CMAKE_PATH="$USD_SRC_PATH/CMakeLists.txt"
+
+    # Specify the line to add
+    LINE_TO_ADD="add_subdirectory(plugin)"
+
+    # Check if the line already exists in the file
+    if ! grep -Fxq "$LINE_TO_ADD" "$USD_CMAKE_PATH"; then
+
+        # Add the line to the file
+        echo "\n$LINE_TO_ADD" >> $USD_CMAKE_PATH
+    fi
+
+    python3 $USD_SRC_PATH/build_scripts/build_usd.py $USD_BUILD_PATH
+fi
