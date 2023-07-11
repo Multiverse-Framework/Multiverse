09436a6d4e81ddf1b97614a7c1cd789cdada3a1f
Giang Nguyen <hoanggia@uni-bremen.de>
2023-07-11 13:43:48 +0200

    Clean up bash files

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ apt install ros-noetic-desktop-full
 # Install python3-pip
 apt install -y python3-pip
 
-# Install python3-dev
-apt install -y python3-dev
+# Install python3.9-dev
+apt install -y python3.9-dev
 
 # Install python3-catkin-tools
 apt install -y python3-catkin-tools
--- multiverse_ws/src/multiverse_core/multiverse_knowledge/install.sh
+++ multiverse_ws/src/multiverse_core/multiverse_knowledge/install.sh
@@ -7,16 +7,20 @@ if [ -z "$USD_SRC_PATH" ] || [ -z "$USD_BUILD_PATH" ]; then
 else
     echo "USD_SRC_PATH is set to: $USD_SRC_PATH"
     echo "USD_BUILD_PATH is set to: $USD_BUILD_PATH"
-
+    
     DEVEL_PATH="$(dirname $0)/../../../devel"
-    PYTHONPATH=$PYTHONPATH:$DEVEL_PATH/lib/python3/dist-packages
     SETUP_PATH="$DEVEL_PATH/setup.sh"
-
-    export PATH=$PATH:$USD_BUILD_PATH/bin
-    export PYTHONPATH=$PYTHONPATH:$USD_BUILD_PATH/lib/python
-
+    
+    if ! echo "$PATH" | grep -q "$USD_BUILD_PATH/bin"; then
+        export PATH=$PATH:$USD_BUILD_PATH/bin
+    fi
+    
+    if ! echo "$PYTHONPATH" | grep -q "$USD_BUILD_PATH/lib/python"; then
+        export PYTHONPATH=$PYTHONPATH:$USD_BUILD_PATH/lib/python
+    fi
+    
     cp -r USD/plugin $USD_SRC_PATH
-
+    
     # Loop through each folder in the directory
     for USD_PLUGIN in $USD_SRC_PATH/plugin/*; do
         if [ -d "$USD_PLUGIN" ]; then
@@ -24,27 +28,33 @@ else
             (cd "$USD_PLUGIN" && usdGenSchema schema.usda)
         fi
     done
-
+    
     # Specify the file path
     USD_CMAKE_PATH="$USD_SRC_PATH/CMakeLists.txt"
-
+    
     # Specify the line to add
     LINE_TO_ADD="add_subdirectory(plugin)"
-
+    
     # Check if the line already exists in the file
     if ! grep -Fxq "$LINE_TO_ADD" "$USD_CMAKE_PATH"; then
-
         # Add the line to the file
         echo "\n$LINE_TO_ADD" >> $USD_CMAKE_PATH
     fi
-
+    
     python3 $USD_SRC_PATH/build_scripts/build_usd.py $USD_BUILD_PATH
-
+    
+    PATH_TO_ADD="if ! echo \"\$PATH\" | grep -q \"$USD_BUILD_PATH/bin\"; then\n  export PATH=\$PATH:$USD_BUILD_PATH/bin\nfi"
+    
     # Check if the line already exists in the file
-    if ! grep -Fxq "export PATH=" "$SETUP_PATH"; then
-
+    if ! grep -Fxq "$PATH_TO_ADD" "$SETUP_PATH"; then
         # Add the line to the file
-        echo "export PATH=$PATH" >> $SETUP_PATH
-        echo "export PYTHONPATH=$PYTHONPATH" >> $SETUP_PATH
+        echo "$PATH_TO_ADD" >> $SETUP_PATH
+    fi
+    
+    PYTHONPATH_TO_ADD="if ! echo \"\$PYTHONPATH\" | grep -q \"$USD_BUILD_PATH/lib/python\"; then\n  export PYTHONPATH=\$PYTHONPATH:$USD_BUILD_PATH/lib/python\nfi"
+    # Check if the line already exists in the file
+    if ! grep -Fxq "$PYTHONPATH_TO_ADD" "$SETUP_PATH"; then
+        # Add the line to the file
+        echo "$PYTHONPATH_TO_ADD" >> $SETUP_PATH
     fi
 fi
