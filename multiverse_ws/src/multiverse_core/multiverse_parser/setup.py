522d8bfef56c69aeb763025ee0f90c7d4d70122d
Giang Nguyen <hoanggia@uni-bremen.de>
2023-07-23 19:10:50 +0200

    Auto format

--- multiverse_ws/src/multiverse_core/multiverse_parser/setup.py
+++ multiverse_ws/src/multiverse_core/multiverse_parser/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["mjcf_to_usd","usd_to_urdf", "sdf_to_urdf"],
+    packages=["mjcf_to_usd", "usd_to_urdf", "sdf_to_urdf"],
     package_dir={
         "mjcf_to_usd": "mjcf_to_usd/scripts/mjcf_to_usd",
         "usd_to_urdf": "usd_to_urdf/scripts/usd_to_urdf",
