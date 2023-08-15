d18b237f95decc6ddbee8f3fe924863e80f2c171
Giang Nguyen <hoanggia@uni-bremen.de>
2023-08-15 19:38:18 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Setup your sources.list
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt-get/sources.list.d/ros-latest.list'
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-get install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
