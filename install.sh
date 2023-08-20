33a06cc50a687dd851ccb6f6beaf1526cbf29fb1
Giang Nguyen <hoanggia@uni-bremen.de>
2023-08-20 17:22:56 +0200

    Add ppa:ubuntu-toolchain-r/test

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ apt-get update
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-get install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+add-apt-repository ppa:ubuntu-toolchain-r/test
 
 # Update package lists
 apt-get update
