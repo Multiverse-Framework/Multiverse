897611bb7cf2bf84d1b9eccd4e3c0bc35a71d806
Giang Nguyen <hoanggia@uni-bremen.de>
2023-10-19 22:18:38 +0200

    Fix again...

--- build_third_parties.sh
+++ build_third_parties.sh
@@ -35,9 +35,9 @@ else
 fi
 
 if [ ! -d "$BLENDER_EXT_DIR/lib" ]; then
-    (cd $BLENDER_EXT_DIR; mkdir lib)
+    (cd $BLENDER_EXT_DIR; mkdir lib; cd lib; svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/lib/linux_x86_64_glibc_228)
     (cd $BLENDER_EXT_DIR/blender; make update)
-    (cd $BLENDER_EXT_DIR/lib; svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/lib/linux_x86_64_glibc_228@63459)
+    (cd $BLENDER_EXT_DIR/lib/linux_x86_64_glibc_228; svn update -r63459)
 fi
 
 (cd $BLENDER_EXT_DIR/blender && make BUILD_DIR=../../../build/blender)
