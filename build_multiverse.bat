207edaacbc91d2615b9f4c8ad2c49a7a7cf67614
Giang Hoang Nguyen <hoanggia@uni-bremen.de>
2024-02-09 13:55:03 +0100

    Update for windows

--- build_multiverse.bat
+++ build_multiverse.bat
@@ -4,12 +4,7 @@ set "MULTIVERSE_DIR=%~dp0multiverse"
 
 rmdir /s /q "%MULTIVERSE_DIR%\build"
 cmake -S "%MULTIVERSE_DIR%" -B "%MULTIVERSE_DIR%\build" -DCMAKE_TOOLCHAIN_FILE"=%MULTIVERSE_DIR%\external\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_INSTALL_PREFIX:PATH="%MULTIVERSE_DIR%"
-cmake --build "%MULTIVERSE_DIR%\build"
+cmake --build "%MULTIVERSE_DIR%\build" --config Debug --target INSTALL
 
-rmdir /s /q "%MULTIVERSE_DIR%\bin"
-mkdir "%MULTIVERSE_DIR%\bin"
-mklink "%MULTIVERSE_DIR%\bin\multiverse_server.exe" "%MULTIVERSE_DIR%\build\src\multiverse_server\Debug\multiverse_server.exe"
-
-rmdir /s /q "%MULTIVERSE_DIR%\lib"
-mkdir "%MULTIVERSE_DIR%\lib"
-copy "%MULTIVERSE_DIR%\build\src\multiverse_client\Debug\multiverse_client.dll" "%MULTIVERSE_DIR%\lib\libmultiverse_client.dll"
+copy /y "%MULTIVERSE_DIR%\build\src\multiverse_server\Debug\jsoncpp.dll" "%MULTIVERSE_DIR%\bin"
+copy /y "%MULTIVERSE_DIR%\build\src\multiverse_server\Debug\libzmq-mt-gd-4_3_5.dll" "%MULTIVERSE_DIR%\bin"
