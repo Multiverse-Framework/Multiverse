2364e285b8ee3ae880d3f200664457508882e36b
Giang Nguyen <hoanggia@uni-bremen.de>
2024-04-21 15:43:19 +0200

    Add git-lfs in install.sh

--- install.sh
+++ install.sh
@@ -69,8 +69,8 @@ sudo update-alternatives --remove-all gcc
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
 
 # Install additional packages for blender
-sudo apt-get install -y build-essential git subversion cmake libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev
-sudo apt-get install -y libwayland-dev wayland-protocols libxkbcommon-dev libdbus-1-dev linux-libc-dev libepoxy-dev
+sudo apt-get install -y build-essential git git-lfs subversion cmake libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev
+sudo apt-get install -y libwayland-dev wayland-protocols libxkbcommon-dev libdbus-1-dev linux-libc-dev
 
 # Install pybind11
 sudo apt-get install -y pybind11-dev
