5d61c74841c39cef90f4e830fa9e9ccc0a0c6aa7
Giang Nguyen <hoanggia@uni-bremen.de>
2024-05-10 14:50:23 +0200

    Add ubuntu version checking

--- install.sh
+++ install.sh
@@ -6,41 +6,43 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Install presiqisite for ubuntu
 sudo apt-get install -y software-properties-common curl python-is-python3
 
-# Setup your sources.list
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-sudo add-apt-repository universe
-
-# Update package lists
-sudo apt-get update
-
-# Install python3.8
-sudo apt-get install -y python3.8-dev python3.8-venv
-sudo update-alternatives --remove-all python3
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 100
-
-# Install ROS1
-sudo apt-get install -y ros-noetic-desktop-full
-sudo apt-get install -y ros-noetic-xacro ros-noetic-rviz ros-noetic-joint-trajectory-controller ros-noetic-rqt-robot-steering ros-noetic-rqt-joint-trajectory-controller ros-noetic-joint-state-controller ros-noetic-joint-state-publisher-gui ros-noetic-effort-controllers ros-noetic-gripper-action-controller ros-noetic-dwa-local-planner ros-noetic-cob-gazebo-objects ros-noetic-map-server ros-noetic-move-base
-sudo apt-get install -y python3-catkin-tools
-
-# Install ROS2
-sudo apt-get install -y ros-foxy-desktop
-sudo apt-get install -y ros-foxy-joint-state-publisher-gui
-sudo apt-get install -y ros-dev-tools
-
 # Install python3-pip
 sudo apt-get install -y python3-pip
 
-# Install rosdep
-sudo apt-get install -y python3-rosdep
-sudo rosdep init
-sudo rosdep fix-permissions
-rosdep update
+if [ $UBUNTU_VERSION = "20.04" ]; then
+    # Setup your sources.list
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+    sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+    sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+    sudo add-apt-repository universe
+
+    # Update package lists
+    sudo apt-get update
+
+    # Install python3.8
+    sudo apt-get install -y python3.8-dev python3.8-venv
+    sudo update-alternatives --remove-all python3
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 100
+
+    # Install ROS1
+    sudo apt-get install -y ros-noetic-desktop-full
+    sudo apt-get install -y ros-noetic-xacro ros-noetic-rviz ros-noetic-joint-trajectory-controller ros-noetic-rqt-robot-steering ros-noetic-rqt-joint-trajectory-controller ros-noetic-joint-state-controller ros-noetic-joint-state-publisher-gui ros-noetic-effort-controllers ros-noetic-gripper-action-controller ros-noetic-dwa-local-planner ros-noetic-cob-gazebo-objects ros-noetic-map-server ros-noetic-move-base
+    sudo apt-get install -y python3-catkin-tools
+
+    # Install ROS2
+    sudo apt-get install -y ros-foxy-desktop
+    sudo apt-get install -y ros-foxy-joint-state-publisher-gui
+    sudo apt-get install -y ros-dev-tools
+
+    # Install rosdep
+    sudo apt-get install -y python3-rosdep
+    sudo rosdep init
+    sudo rosdep fix-permissions
+    rosdep update
+fi
 
 # Install glfw3
 sudo apt-get install -y libglfw3
@@ -64,21 +66,29 @@ sudo apt-get install -y libxcb-cursor0
 # Install additional packages for MuJoCo
 sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev libxt-dev
 
-# Install and link clang-17 for creating shared library
-sudo apt-get install -y clang-17 llvm-17-dev libc++-17-dev libc++abi-17-dev libstdc++-14-dev 
-sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
-sudo update-alternatives --remove-all clang++
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-17 100
+if [ $UBUNTU_VERSION = "20.04" ]; then
+    # Install and link clang-11 for creating shared library
+    sudo apt-get install -y clang-11 llvm-11-dev libc++-11-dev libc++abi-11-dev libstdc++-11-dev 
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
+    sudo update-alternatives --remove-all clang++
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-11 100
 
-# Install and link gcc-11
-sudo apt-get install -y gcc-11
-sudo update-alternatives --remove-all gcc
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
+    # Install and link gcc-11
+    sudo apt-get install -y gcc-11
+    sudo update-alternatives --remove-all gcc
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
 
-# Install and link gcc+11
-sudo apt-get install -y g++-11
-sudo update-alternatives --remove-all g++
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
+    # Install and link gcc+11
+    sudo apt-get install -y g++-11
+    sudo update-alternatives --remove-all g++
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
+else if [ $UBUNTU_VERSION = "24.04" ]; then
+    # Install and link clang-17 for creating shared library
+    sudo apt-get install -y clang-17 llvm-17-dev libc++-17-dev libc++abi-17-dev libstdc++-14-dev 
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
+    sudo update-alternatives --remove-all clang++
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-17 100
+fi
 
 # Install additional packages for blender
 sudo apt-get install -y build-essential git git-lfs subversion cmake libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev
