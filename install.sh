e40cd31084b7e77971480f455eba73fc19f79bcd
Giang Nguyen <hoanggia@uni-bremen.de>
2024-06-02 14:52:39 +0200

    Remove update and upgrade

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Update package lists
-sudo apt-get update
+# sudo apt-get update && sudo apt-get upgrade -y
 
 # Install presiqisite for ubuntu
 sudo apt-get install -y software-properties-common curl python-is-python3
