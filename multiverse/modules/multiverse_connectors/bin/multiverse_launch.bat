1b62062d07c6c9c843430cefd450eef8c10794b1
Giang Hoang Nguyen <hoanggia@uni-bremen.de>
2024-07-17 22:22:27 +0200

    Update for windows 11

--- multiverse/modules/multiverse_connectors/bin/multiverse_launch.bat
+++ multiverse/modules/multiverse_connectors/bin/multiverse_launch.bat
@@ -11,14 +11,14 @@ if "%~1"=="" (
 )
 
 set "MUV_FILE=%~1"
+set "PYTHON_EXECUTABLE=%USERPROFILE%\Envs\multiverse\Scripts\python.exe"
 if not exist %MUV_FILE% (
     echo Error: File %MUV_FILE% not found.
     exit /b 1
 )
 
-echo %MULTIVERSE_PATH%
-python "%MULTIVERSE_PATH%\modules\multiverse_connectors\scripts\launch_multiverse_server.py" --muv_file="%MUV_FILE%"
-python "%MULTIVERSE_PATH%\modules\multiverse_connectors\scripts\launch_simulators.py" --muv_file="%MUV_FILE%"
+%PYTHON_EXECUTABLE% %MULTIVERSE_PATH%\modules\multiverse_connectors\scripts\launch_multiverse_server.py --muv_file=%MUV_FILE%
+%PYTHON_EXECUTABLE% %MULTIVERSE_PATH%\modules\multiverse_connectors\scripts\launch_simulators.py --muv_file=%MUV_FILE%
 
 echo [multiverse_launch] Running... Press Ctrl+C to exit
 :loop
