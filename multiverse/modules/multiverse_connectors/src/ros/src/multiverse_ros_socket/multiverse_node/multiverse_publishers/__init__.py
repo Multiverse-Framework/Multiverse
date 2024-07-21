b73f792798fe406828962f111a7c950349c05ac8
Giang Nguyen <hoanggia@uni-bremen.de>
2024-07-21 12:37:32 +0200

    Add image_depth_publisher

--- multiverse/modules/multiverse_connectors/src/ros/src/multiverse_ros_socket/multiverse_node/multiverse_publishers/__init__.py
+++ multiverse/modules/multiverse_connectors/src/ros/src/multiverse_ros_socket/multiverse_node/multiverse_publishers/__init__.py
@@ -1,3 +1,4 @@
 from .tf_publisher import TfPublisher
 from .odom_publisher import OdomPublisher
-from .camera_publisher import CameraPublisher
+from .image_rgb_publisher import ImageRgbPublisher
+from .image_depth_publisher import ImageDepthPublisher
---
+++ multiverse/modules/multiverse_connectors/src/ros/src/multiverse_ros_socket/multiverse_node/multiverse_publishers/image_rgb_publisher.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+from typing import List, Dict
+import numpy
+
+from sensor_msgs.msg import Image
+
+from .multiverse_publisher import Interface, INTERFACE
+
+if INTERFACE == Interface.ROS1:
+    import rospy
+
+from .multiverse_publisher import MultiversePublisher, SocketAddress, MultiverseMetaData
+
+
+class ImageRgbPublisher(MultiversePublisher):
+    _use_meta_data = False
+    _msg_type = Image
+    _body_name: str
+    _frame_id: str
+
+    def __init__(
+        self,
+        client_addr: SocketAddress,
+        topic_name: str = "/camera/rgb/image_raw",
+        rate: float = 60.0,
+        multiverse_meta_data: MultiverseMetaData = MultiverseMetaData(),
+        **kwargs: Dict,
+    ) -> None:
+        super().__init__(
+            client_addr=client_addr,
+            topic_name=topic_name,
+            rate=rate,
+            multiverse_meta_data=multiverse_meta_data,
+        )
+        if "camera" not in kwargs:
+            raise Exception("Camera not found.")
+        if "image_res" not in kwargs:
+            raise Exception("Image resolution not found.")
+        self._camera_name = str(kwargs["camera"])
+        self._frame_id = str(kwargs.get("frame_id", "map"))
+        image_res = str(kwargs["image_res"])
+        if image_res == "3840_2160":
+            self._msg.width = 3840
+            self._msg.height = 2160
+            self.request_meta_data["receive"][self._camera_name] = ["rgb_3840_2160"]
+        elif image_res == "1280_1024":
+            self._msg.width = 1280
+            self._msg.height = 1024
+            self.request_meta_data["receive"][self._camera_name] = ["rgb_1280_1024"]
+        elif image_res == "640_480":
+            self._msg.width = 640
+            self._msg.height = 480
+            self.request_meta_data["receive"][self._camera_name] = ["rgb_640_480"]
+        elif image_res == "128_128":
+            self._msg.width = 128
+            self._msg.height = 128
+            self.request_meta_data["receive"][self._camera_name] = ["rgb_128_128"]
+        else:
+            raise Exception(f"Invalid image resolution {image_res}.")
+        
+        if INTERFACE == Interface.ROS1:
+            self._msg.header.stamp = rospy.Time.now()
+            self._msg.header.seq = 0
+        elif INTERFACE == Interface.ROS2:
+            self._msg.header.stamp = self.get_clock().now().to_msg()
+        self._msg.header.frame_id = self._frame_id
+        
+        self._msg.encoding = "rgb8"
+        self._msg.is_bigendian = False
+        self._msg.step = 3
+
+    def _bind_response_meta_data(self, response_meta_data) -> None:
+        if response_meta_data.get("receive") is None:
+            return
+
+    def _bind_receive_data(self, receive_data: List[float]) -> None:
+        if len(receive_data) != self._msg.height * self._msg.width * 3 + 1:
+            self.logwarn(
+                f"Invalid data length {len(receive_data)} for camera {self._camera_name}."
+            )
+            return
+
+        if INTERFACE == Interface.ROS1:
+            self._msg.header.stamp = rospy.Time.now()
+            self._msg.header.seq += 1
+        elif INTERFACE == Interface.ROS2:
+            self._msg.header.stamp = self.get_clock().now().to_msg()
+        
+        data_array = numpy.array(receive_data[1:], dtype=numpy.uint8)
+        self._msg.data = data_array.tobytes()
