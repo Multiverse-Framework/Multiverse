5cb8ce81f9415fbda2e565eb324f6a1401af8aa6
Giang Nguyen <hoanggia@uni-bremen.de>
2024-07-24 16:40:57 +0200

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
 import subprocess
+import sys
+
+sys.path.insert(0, '_static/lib')
+sys.path.insert(0, '../../multiverse/modules/multiverse_connectors/src/multiverse_client_py/src')
 
 print('****************************************')
 print('conf.py')
