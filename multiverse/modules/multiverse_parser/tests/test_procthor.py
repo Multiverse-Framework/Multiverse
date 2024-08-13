460720123d0fc87c91d0f909bdb4e0d17d1af901
Giang Nguyen <hoanggia@uni-bremen.de>
2024-08-13 17:29:25 +0200

    Update procthor

--- multiverse/modules/multiverse_parser/tests/test_procthor.py
+++ multiverse/modules/multiverse_parser/tests/test_procthor.py
@@ -3,7 +3,7 @@ import unittest
 import os
 import re
 import json
-from typing import Dict, Any
+from typing import Dict, Any, List
 import numpy
 from scipy.spatial.transform import Rotation
 from multiverse_parser import Configuration, InertiaSource, Factory
@@ -19,7 +19,7 @@ from pxr import Usd, UsdGeom, UsdPhysics
 from mujoco import MjModel, mjMINVAL
 from urdf_parser_py import urdf
 
-house_name = "house_5"
+house_name = "house_69"
 
 source_dir = "/media/giangnguyen/Storage/dataset/"
 
@@ -50,12 +50,13 @@ class ProcthorImporter(Factory):
             self.import_object(house_name, obj)
 
         walls = house["walls"]
-        for wall_id, wall in enumerate(walls):
-            self.import_wall(wall, wall_id)
-
         doors = house["doors"]
+        walls_with_door = {}
         for door_id, door in enumerate(doors):
-            self.import_door(door, door_id)
+            self.import_door(door, door_id, walls, walls_with_door)
+
+        for wall_id, wall in enumerate(walls):
+            self.import_wall(wall, wall_id, walls_with_door)
 
     def import_object(self, parent_body_name: str, obj: Dict[str, Any]) -> None:
         body_name = obj["id"].replace("|", "_")
@@ -86,7 +87,7 @@ class ProcthorImporter(Factory):
         for child in obj.get("children", {}):
             self.import_object(body_name, child)
 
-    def import_wall(self, wall: Dict[str, Any], wall_id: int) -> None:
+    def import_wall(self, wall: Dict[str, Any], wall_id: int, walls_with_door: Dict[str, Any]) -> None:
         body_name = f"Wall_{wall_id}"
         body_builder = self._world_builder.add_body(body_name=body_name, parent_body_name=house_name)
 
@@ -110,27 +111,6 @@ class ProcthorImporter(Factory):
         normal = (normal_1 + normal_2) / 2
         normal /= numpy.linalg.norm(normal)
 
-        if normal[0] != 0:
-            x = point_1[0]
-            dx = -0.05 * normal[0]
-            y = (point_1[1] + point_3[1]) / 2.0
-            dy = (point_3[1] - point_1[1]) / 2.0
-            z = (point_1[2] + point_2[2]) / 2.0
-            dz = (point_2[2] - point_1[2]) / 2.0
-        elif normal[2] != 0:
-            x = (point_2[0] + point_3[0]) / 2.0
-            dx = (point_3[0] - point_2[0]) / 2.0
-            y = (point_1[1] + point_3[1]) / 2.0
-            dy = (point_3[1] - point_1[1]) / 2.0
-            z = point_1[2]
-            dz = -0.05 * normal[2]
-        else:
-            raise ValueError("Invalid normal")
-
-        points = numpy.array(
-            [[x - dx, y - dy, z - dz], [x - dx, y - dy, z + dz], [x - dx, y + dy, z - dz], [x - dx, y + dy, z + dz],
-             [x + dx, y - dy, z - dz], [x + dx, y - dy, z + dz], [x + dx, y + dy, z - dz],
-             [x + dx, y + dy, z + dz]])
         normals = numpy.array([[-1, 0, 0], [-1, 0, 0], [-1, 0, 0],
                                [0, 1, 0], [0, 1, 0], [0, 1, 0],
                                [1, 0, 0], [1, 0, 0], [1, 0, 0],
@@ -152,40 +132,155 @@ class ProcthorImporter(Factory):
              [7, 5, 4], [5, 1, 0],
              [6, 4, 0], [3, 1, 5]])
 
-        mesh_file_name = f"Wall_{wall_id}"
+        if wall["id"] not in walls_with_door:
+            if normal[0] != 0:
+                x = point_1[0]
+                dx = -0.05 * normal[0]
+                y = (point_1[1] + point_3[1]) / 2.0
+                dy = (point_3[1] - point_1[1]) / 2.0
+                z = (point_1[2] + point_2[2]) / 2.0
+                dz = (point_2[2] - point_1[2]) / 2.0
+            elif normal[2] != 0:
+                x = (point_2[0] + point_3[0]) / 2.0
+                dx = (point_3[0] - point_2[0]) / 2.0
+                y = (point_1[1] + point_3[1]) / 2.0
+                dy = (point_3[1] - point_1[1]) / 2.0
+                z = point_1[2]
+                dz = -0.05 * normal[2]
+            else:
+                raise ValueError("Invalid normal")
+            points = numpy.array(
+                [[x - dx, y - dy, z - dz], [x - dx, y - dy, z + dz], [x - dx, y + dy, z - dz], [x - dx, y + dy, z + dz],
+                 [x + dx, y - dy, z - dz], [x + dx, y - dy, z + dz], [x + dx, y + dy, z - dz], [x + dx, y + dy, z + dz]])
 
-        mesh_property = MeshProperty(points=points,
-                                     normals=normals,
-                                     face_vertex_counts=face_vertex_counts,
-                                     face_vertex_indices=face_vertex_indices,
-                                     mesh_file_name=mesh_file_name)
-        geom_property = GeomProperty(geom_type=GeomType.MESH,
-                                     is_visible=True,
-                                     is_collidable=True)
-        geom_builder = body_builder.add_geom(geom_name=f"{body_name}",
-                                             geom_property=geom_property)
-        geom_builder.add_mesh(mesh_name=f"SM_{body_name}", mesh_property=mesh_property)
+            mesh_file_name = f"Wall_{wall_id}"
 
-    def import_door(self, door: Dict[str, Any], door_id : int) -> None:
+            mesh_property = MeshProperty(points=points,
+                                         normals=normals,
+                                         face_vertex_counts=face_vertex_counts,
+                                         face_vertex_indices=face_vertex_indices,
+                                         mesh_file_name=mesh_file_name)
+            geom_property = GeomProperty(geom_type=GeomType.MESH,
+                                         is_visible=True,
+                                         is_collidable=True)
+            geom_builder = body_builder.add_geom(geom_name=f"{body_name}",
+                                                 geom_property=geom_property)
+            geom_builder.add_mesh(mesh_name=f"SM_{body_name}", mesh_property=mesh_property)
+
+        else:
+            door = walls_with_door[wall["id"]]
+            hole_height = door["holePolygon"][1]["y"]
+            if point_1[1] < point_3[1]:
+                y = [(point_1[1] + hole_height) / 2.0, (hole_height + point_3[1]) / 2.0, (point_1[1] + hole_height) / 2.0]
+                dy = [(hole_height - point_1[1]) / 2.0, (point_3[1] - hole_height) / 2.0, (hole_height - point_1[1]) / 2.0]
+            else:
+                y = [(point_3[1] + hole_height) / 2.0, (hole_height + point_1[1]) / 2.0, (point_3[1] + hole_height) / 2.0]
+                dy = [(hole_height - point_3[1]) / 2.0, (point_1[1] - hole_height) / 2.0, (hole_height - point_3[1]) / 2.0]
+            if normal[0] != 0:
+                z = [(point_1[2] + point_2[2]) / 2.0] * 3
+                dz = [(point_2[2] - point_1[2]) / 2.0] * 3
+
+                dz[0] = door["holePolygon"][0]["y"] / 2.0
+                if point_1[2] > point_2[2]:
+                    z[0] = point_2[2] + dz[0]
+                    dz[2] = (point_1[2] - point_2[2] - door["holePolygon"][1]["y"]) / 2.0
+                    z[2] = point_1[2] - dz[2]
+                else:
+                    z[0] = point_1[2] + dz[0]
+                    dz[2] = (point_2[2] - point_1[2] - door["holePolygon"][1]["y"]) / 2.0
+                    z[2] = point_2[2] - dz[2]
+
+                x = [point_1[0]] * 3
+                dx = [-0.05 * normal[0]] * 3
+            elif normal[2] != 0:
+                x = [(point_2[0] + point_3[0]) / 2.0] * 3
+                dx = [(point_3[0] - point_2[0]) / 2.0] * 3
+
+                dx[0] = door["holePolygon"][0]["x"] / 2.0
+                if point_2[0] > point_3[0]:
+                    x[0] = point_3[0] + dx[0]
+                    dx[2] = (point_2[0] - point_3[0] - door["holePolygon"][1]["x"]) / 2.0
+                    x[2] = point_2[0] - dx[2]
+                else:
+                    x[0] = point_2[0] + dx[0]
+                    dx[2] = (point_3[0] - point_2[0] - door["holePolygon"][1]["x"]) / 2.0
+                    x[2] = point_3[0] - dx[2]
+
+                z = [point_1[2]] * 3
+                dz = [-0.05 * normal[2]] * 3
+            else:
+                raise ValueError("Invalid normal")
+
+            for idx in range(3):
+                points = numpy.array(
+                    [[x[idx] - dx[idx], y[idx] - dy[idx], z[idx] - dz[idx]], [x[idx] - dx[idx], y[idx] - dy[idx], z[idx] + dz[idx]],
+                     [x[idx] - dx[idx], y[idx] + dy[idx], z[idx] - dz[idx]], [x[idx] - dx[idx], y[idx] + dy[idx], z[idx] + dz[idx]],
+                     [x[idx] + dx[idx], y[idx] - dy[idx], z[idx] - dz[idx]], [x[idx] + dx[idx], y[idx] - dy[idx], z[idx] + dz[idx]],
+                     [x[idx] + dx[idx], y[idx] + dy[idx], z[idx] - dz[idx]], [x[idx] + dx[idx], y[idx] + dy[idx], z[idx] + dz[idx]]])
+
+                mesh_file_name = f"Wall_{wall_id}_{idx}"
+
+                mesh_property = MeshProperty(points=points,
+                                             normals=normals,
+                                             face_vertex_counts=face_vertex_counts,
+                                             face_vertex_indices=face_vertex_indices,
+                                             mesh_file_name=mesh_file_name)
+                geom_property = GeomProperty(geom_type=GeomType.MESH,
+                                             is_visible=True,
+                                             is_collidable=True)
+                geom_builder = body_builder.add_geom(geom_name=f"{body_name}_{idx}",
+                                                     geom_property=geom_property)
+                geom_builder.add_mesh(mesh_name=f"SM_{body_name}_{idx}", mesh_property=mesh_property)
+
+    def import_door(self, door: Dict[str, Any], door_id: int, walls: List[Dict[str, Any]], walls_with_door: Dict) -> None:
         body_name = f"Door_{door_id}"
         body_builder = self._world_builder.add_body(body_name=body_name, parent_body_name=house_name)
 
         position = door.get("assetPosition", {"x": 0, "y": 0, "z": 0})
-        position_vec = numpy.array([position["x"], position["z"], position["y"]])
+        position_vec = numpy.array([0, 0, position["y"]])
 
-        x_90_rotation_matrix = numpy.array([[1, 0, 0],
-                                            [0, 0, -1],
-                                            [0, 1, 0]])
-        rotation_quat = Rotation.from_matrix(x_90_rotation_matrix).as_quat()
+        wall0 = door.get("wall0", None)
+        wall1 = door.get("wall1", None)
+        if wall0 is None:
+            raise ValueError(f"Door {door_id} does not have wall0")
+        if wall1 is None:
+            raise ValueError(f"Door {door_id} does not have wall1")
+        
+        for wall in walls:
+            if wall["id"] == wall1:
+                walls_with_door[wall1] = door
+                break
+        else:
+            raise ValueError(f"Wall {wall1} not found")
+        
+        for wall in walls:
+            if wall["id"] == wall0:
+                walls_with_door[wall0] = door
+                break
+        else:
+            raise ValueError(f"Wall {wall0} not found")
 
-        body_builder.set_transform(pos=position_vec)
+        if wall["polygon"][0]["x"] == wall["polygon"][1]["x"]:
+            position_vec[0] = position["z"] + wall["polygon"][0]["x"]
+            position_vec[1] = -position["x"] - (
+                wall["polygon"][0]["z"] if wall["polygon"][0]["z"] < wall["polygon"][1]["z"] else wall["polygon"][1]["z"])
+            body_builder.set_transform(pos=position_vec,
+                                       quat=Rotation.from_euler("xyz", [0, 0, 90], degrees=True).as_quat())
+        elif wall["polygon"][0]["z"] == wall["polygon"][1]["z"]:
+            position_vec[0] = position["x"] + (
+                wall["polygon"][0]["x"] if wall["polygon"][0]["x"] < wall["polygon"][1]["x"] else wall["polygon"][1]["x"])
+            position_vec[1] = position["z"] - wall["polygon"][0]["z"]
+            body_builder.set_transform(pos=position_vec)
+        else:
+            raise ValueError(f"Invalid wall {wall}")
 
         if "assetId" not in door:
             return None
 
         asset_id = door["assetId"]
         asset_id = re.sub(r'(\d+)x(\d+)', r'\1_X_\2', asset_id)
-        self.import_asset(body_builder, asset_id)
+        self.import_asset(body_builder, asset_id)            
+        
 
     def import_asset(self, body_builder: BodyBuilder, asset_name: str) -> None:
         asset_name = snake_to_camel(asset_name)
@@ -314,10 +409,10 @@ class TestProcthor(unittest.TestCase):
         factory.save_tmp_model(house_usd_file_path)
 
         # Export to MJCF
-        # exporter = MjcfExporter(file_path=house_usd_file_path.replace(".usda", ".xml"),
-        #                         factory=factory)
-        # exporter.build()
-        # exporter.export(keep_usd=False)
+        exporter = UrdfExporter(file_path=house_usd_file_path.replace(".usda", ".urdf"),
+                                factory=factory)
+        exporter.build()
+        exporter.export(keep_usd=False)
 
 
 if __name__ == '__main__':
