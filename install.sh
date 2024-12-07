cdf29aa2fca68f358b64b03862acd60140d8981b
Giang Nguyen <hoanggia@uni-bremen.de>
2024-12-07 13:40:48 +0100

    Add repository for python3.10

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ UBUNTU_VERSION=$(lsb_release -rs)
 
 if [ $UBUNTU_VERSION = "20.04" ]; then
     # Setup your sources.list
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa
+
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test
