e95f51a0410574c95001fea9a58cc843bb61df7d
Giang Nguyen <hoanggia@uni-bremen.de>
2024-12-10 11:39:49 +0100

    Update for ubuntu 22.04

--- install.sh
+++ install.sh
@@ -129,8 +129,8 @@ if [ $UBUNTU_VERSION = "20.04" ]; then
     # Setup virtual environment
     pip install virtualenvwrapper
 elif [ $UBUNTU_VERSION = "22.04" ]; then
-    # Install and link clang-17 for creating shared library
-    sudo apt-get install -y clang-17 llvm-17-dev libc++-17-dev libc++abi-17-dev libstdc++-14-dev 
+    # Install and link clang-14 for creating shared library
+    sudo apt-get install -y clang-14 llvm-14-dev libc++-14-dev libc++abi-14-dev libstdc++-12-dev 
     sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
     sudo update-alternatives --remove-all clang++
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-17 100
@@ -142,7 +142,7 @@ elif [ $UBUNTU_VERSION = "24.04" ]; then
     sudo apt-get install -y clang-17 llvm-17-dev libc++-17-dev libc++abi-17-dev libstdc++-14-dev 
     sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
     sudo update-alternatives --remove-all clang++
-    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-17 100
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-14 100
 
     # Setup virtual environment
     python3 -m pip install virtualenvwrapper --break-system-packages
