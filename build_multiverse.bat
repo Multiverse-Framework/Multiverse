17e2f910cd4396e4a1336a905386117dbb3a9df0
Giang Nguyen <hoanggia@uni-bremen.de>
2024-12-26 21:08:36 +0100

    Fix PYTHON_EXECUTE

--- build_multiverse.bat
+++ build_multiverse.bat
@@ -6,6 +6,11 @@ set "MULTIVERSE_DIR=%~dp0multiverse"
 set "CONFIG=Release"
 set "MSYS2_BIN_DIR=%MULTIVERSE_DIR%\external\msys2\mingw64\bin"
 set "CMAKE_BIN_DIR=C:\Program Files\CMake\bin"
+set "PYTHON_EXECUTABLE=%USERPROFILE%\Envs\multiverse\Scripts\python.exe"
+if not exist "%PYTHON_EXECUTABLE%" (
+    echo "Python executable not found: %PYTHON_EXECUTABLE%"
+    exit /b 1
+)
 
 if exist "%MULTIVERSE_DIR%\build\CMakeCache.txt" del /F "%MULTIVERSE_DIR%\build\CMakeCache.txt"
 
@@ -29,7 +34,7 @@ if "%BUILD_WITH_VCPKG%"=="1" (
     )
 
     if exist "%CMAKE_BIN_DIR%\cmake.exe" (
-        powershell -NoProfile -Command "workon multiverse; & '%CMAKE_BIN_DIR%\cmake.exe' -S %MULTIVERSE_DIR% -B %MULTIVERSE_DIR%\build -DCMAKE_TOOLCHAIN_FILE=%MULTIVERSE_DIR%\external\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_INSTALL_PREFIX:PATH=%MULTIVERSE_DIR% -Wno-deprecated -DBUILD_MODULES=OFF"
+        powershell -NoProfile -Command "workon multiverse; & '%CMAKE_BIN_DIR%\cmake.exe' -S %MULTIVERSE_DIR% -B %MULTIVERSE_DIR%\build -DCMAKE_TOOLCHAIN_FILE=%MULTIVERSE_DIR%\external\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_INSTALL_PREFIX:PATH=%MULTIVERSE_DIR% -Wno-deprecated -DBUILD_MODULES=OFF -DPYTHON_EXECUTABLE=%PYTHON_EXECUTABLE%"
         powershell -NoProfile -Command "workon multiverse; & '%CMAKE_BIN_DIR%\cmake.exe' --build %MULTIVERSE_DIR%\build --config %CONFIG% --target INSTALL"
 
         powershell -Command "if (-not ($env:PATH.Split(';') -contains '%MULTIVERSE_DIR%\bin')) {[Environment]::SetEnvironmentVariable('PATH', [Environment]::GetEnvironmentVariable('Path', 'User') + ';%MULTIVERSE_DIR%\bin', [EnvironmentVariableTarget]::User)}"
@@ -50,7 +55,7 @@ if "%BUILD_WITH_MSYS2%"=="1" (
     echo Building with msys2
 
     if exist "%MSYS2_BIN_DIR%\cmake.exe" if exist "%MSYS2_BIN_DIR%\ninja.exe" (
-        powershell -NoProfile -Command "workon multiverse; %MSYS2_BIN_DIR%\cmake.exe -S %MULTIVERSE_DIR% -B %MULTIVERSE_DIR%\build -G Ninja -DCMAKE_MAKE_PROGRAM=%MSYS2_BIN_DIR%\ninja.exe -D CMAKE_C_COMPILER=%MSYS2_BIN_DIR%\gcc.exe -D CMAKE_CXX_COMPILER=%MSYS2_BIN_DIR%\g++.exe -DCMAKE_INSTALL_PREFIX:PATH=%MULTIVERSE_DIR% -Wno-deprecated -Wno-dev -DBUILD_MODULES=ON"
+        powershell -NoProfile -Command "workon multiverse; %MSYS2_BIN_DIR%\cmake.exe -S %MULTIVERSE_DIR% -B %MULTIVERSE_DIR%\build -G Ninja -DCMAKE_MAKE_PROGRAM=%MSYS2_BIN_DIR%\ninja.exe -D CMAKE_C_COMPILER=%MSYS2_BIN_DIR%\gcc.exe -D CMAKE_CXX_COMPILER=%MSYS2_BIN_DIR%\g++.exe -DCMAKE_INSTALL_PREFIX:PATH=%MULTIVERSE_DIR% -Wno-deprecated -Wno-dev -DBUILD_MODULES=ON -DPYTHON_EXECUTABLE=%PYTHON_EXECUTABLE%"
         powershell -NoProfile -Command "workon multiverse; %MSYS2_BIN_DIR%\cmake.exe --build %MULTIVERSE_DIR%\build --config %CONFIG%"
         powershell -NoProfile -Command "workon multiverse; %MSYS2_BIN_DIR%\cmake.exe --install %MULTIVERSE_DIR%\build"
         
