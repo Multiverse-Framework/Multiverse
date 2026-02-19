cae4751ca41fdebcf67f06d8b50644ec8c41148e
Giang Nguyen <hoanggia@uni-bremen.de>
2026-02-19 19:01:48 +0100

    Fix README.md

--- Demos/1_TiagoDualInApartment/README.md
+++ Demos/1_TiagoDualInApartment/README.md
@@ -1,8 +1,5 @@
-### 9️⃣ Download MuJoCo 3.5.0 and Copy Plugins
+# Demo 1: Teleop TiagoDual in Apartment #
 
 ```bash
-export MUJOCO_VERSION=3.5.0
-wget -qO- https://github.com/google-deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux-x86_64.tar.gz | tar -xz -C $PWD/Multiverse/Demos/1_TiagoDualInApartment/
-cp -f ./Multiverse/MultiverseConnector/mujoco_connector/mujoco-${MUJOCO_VERSION}/*.so \
-      ./Multiverse/Demos/1_TiagoDualInApartment/mujoco-${MUJOCO_VERSION}/bin/mujoco_plugin/
+bash run.sh
 ```
--- Demos/1_TiagoDualInApartment/run.sh
+++ Demos/1_TiagoDualInApartment/run.sh
@@ -21,6 +21,22 @@ fi
 
 cd ../..
 
+MUJOCO_VERSION=3.5.0
+if [ ! -d "$PWD/Demos/1_TiagoDualInApartment/mujoco-${MUJOCO_VERSION}" ]; then
+  echo "MuJoCo not found. Downloading MuJoCo ${MUJOCO_VERSION}..."
+  wget -qO- https://github.com/google-deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux-x86_64.tar.gz | tar -xz -C $PWD/Demos/1_TiagoDualInApartment/
+  cp -f ./MultiverseConnector/mujoco_connector/mujoco-${MUJOCO_VERSION}/*.so ./Demos/1_TiagoDualInApartment/mujoco-${MUJOCO_VERSION}/bin/mujoco_plugin/
+fi
+
+ROSPKG_PATH="$PWD"/MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+if [ ! -f "${ROSPKG_PATH}" ]; then
+  echo "ROS package not found. Building it..."
+  source /opt/ros/jazzy/setup.bash
+  cd "$PWD"/MultiverseConnector/ros_connector/ros_ws/multiverse_ws2 || exit
+  colcon build --symlink-install
+  cd ../../../../
+fi
+
 tmux new-session -d -s "$SESH" -n server
 
 tmux set-option -t "$SESH" -g mouse on
@@ -56,20 +72,20 @@ export MUJOCO_VERSION=3.5.0
 # Pane 2 - robot_state_publisher
 tmux send-keys -t "$SESH":0.2 \
 "source /opt/ros/jazzy/setup.bash
-source ./MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${ROSPKG_PATH}
 ros2 run robot_state_publisher robot_state_publisher --ros-args --remap /robot_description:=/robot_description -p robot_description:=\"\$(xacro ./Demos/1_TiagoDualInApartment/assets/urdf/iai_tiago.urdf)\" -r tf:=/tf" C-m
 
 # Pane 3 - ros2_control_node
 tmux send-keys -t "$SESH":0.3 \
 "source /opt/ros/jazzy/setup.bash
-source ./MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${ROSPKG_PATH}
 ros2 run controller_manager ros2_control_node --ros-args --remap /robot_description:=/robot_description --params-file './Demos/1_TiagoDualInApartment/config/ros2_control.yaml'
 " C-m
 
 # Pane 4 - spawn controllers + rviz2
 tmux send-keys -t "$SESH":0.4 \
 "source /opt/ros/jazzy/setup.bash
-source ./MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${ROSPKG_PATH}
 ros2 run controller_manager spawner joint_state_broadcaster upper_body_position_controller --param-file ./Demos/1_TiagoDualInApartment/config/ros2_control.yaml
 cp ./Demos/1_TiagoDualInApartment/assets/urdf/iai_tiago.urdf /tmp/iai_tiago.urdf
 sed -i 's|file://\([^/]\)|file://'"'"'$PWD'"'"'/./Demos/1_TiagoDualInApartment/assets/urdf/\1|g' /tmp/iai_tiago.urdf
@@ -78,14 +94,14 @@ ros2 run rviz2 rviz2 --display-config ./Demos/1_TiagoDualInApartment/config/rviz
 # Pane 5 - vr_teleop_action_server
 tmux send-keys -t "$SESH":0.5 \
 "source /opt/ros/jazzy/setup.bash
-source ./MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${ROSPKG_PATH}
 cd ./Demos/1_TiagoDualInApartment
 ros2 run vr_teleop_action vr_teleop_action_server --ros-args --params-file ./config/vr_teleop.yaml" C-m
 
 # Pane 6 - 
 tmux send-keys -t "$SESH":0.6 \
 "source /opt/ros/jazzy/setup.bash
-source ./MultiverseConnector/ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${ROSPKG_PATH}
 ros2 action send_goal /teleop vr_teleop_interfaces/action/Teleop \"timeout: {sec: -1}\"" C-m
 
 # Pane 7 - 
